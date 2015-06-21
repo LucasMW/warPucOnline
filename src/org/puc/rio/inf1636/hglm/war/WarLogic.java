13bb3e9c20e2893cbd72b7578844a4f5a77f4f56
LucasMW <great-lucas@hotmail.com>
2015-06-21 20:04:41 -0300

    ädded detail from manual

--- src/org/puc/rio/inf1636/hglm/war/WarLogic.java
+++ src/org/puc/rio/inf1636/hglm/war/WarLogic.java
@@ -10,6 +10,10 @@ public class WarLogic {
 	public final static int MAX_DICE = 3;
 	
 	public static int calculateArmiesToGain(Player p) {
-		return p.getNumberOfTerritories() / 2;
+		int  x = p.getNumberOfTerritories() / 2;
+		if(x<3) // that`s the minimum specified in the manual
+			x=3;
+		return x;
+		
 	}
 }
