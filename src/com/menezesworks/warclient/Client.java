ac87251c26c43c4270e8971aafafd1386043560f
LucasMW <great-lucas@hotmail.com>
2015-12-06 14:07:13 -0200

    Client specialization

--- src/com/menezesworks/warclient/Client.java
+++ src/com/menezesworks/warclient/Client.java
@@ -8,35 +8,34 @@ public class Client
 	int socketNumber;
 	String ipLocalhost;
 	public Socket socket;
+	public String name; // same name other player will see
+	ServerListener serverEar; //thread that listen the server
+	PrintStream sendStream; // where data will be sent to the server
 	public Client(int socketNumber)
 	{
 		this.socketNumber = socketNumber;
 		this.ipLocalhost = "127.0.0.1";
 	}
-	public void connect(String ip) throws UnknownHostException, IOException 
+	public void connect(String ip, String identifierName) throws UnknownHostException, IOException 
 	{
 		if(ip == null)
 			ip = this.ipLocalhost;
 		this.socket = new Socket(ip, this.socketNumber);
-		System.out.println("O cliente se conectou ao servidor!");
-		ServerListener serverEar = new ServerListener(this);
+		System.out.println("Connected to server!");
+		this.serverEar = new ServerListener(this);
 		Thread t = new Thread(serverEar);
-		Scanner teclado = new Scanner(System.in);
-		PrintStream saida = new PrintStream(socket.getOutputStream());
-		String msg = teclado.nextLine(); 
+		this.sendStream = new PrintStream(socket.getOutputStream()); 
 		t.start();
-		while (msg.compareTo("###")!=0) 
-		{
-			saida.println(msg); msg = teclado.nextLine();
+		this.name = identifierName;
+		sendStream.println(identifierName); // first data received by server from this client is it's player name
+
 		}
+	public void disconnect() throws IOException
+	{
 		serverEar.stop();
-		saida.close();
-		teclado.close();
+		this.sendStream.close();
 		socket.close();
-
-		System.out.println("O cliente terminou de executar!");
-
-		}
+	}
 
 	public Readable getInputStream() {
 		// TODO Auto-generated method stub
